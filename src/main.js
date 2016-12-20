ba03460b2d9151490e7491b311e56551c6203c06
Leonardo Silveira <sombriks@gmail.com>
2016-12-20 03:11:20 -0300

    .

---
+++ src/main.js
@@ -0,0 +1,2 @@
+
+const vue = require("vue");
---
+++ test/base.js
@@ -0,0 +1,25 @@
+"use strict"
+
+const Nightmare = require("nightmare");
+const expect = require("chai").expect;
+const budo = require("budo");
+
+describe("\u263C basic tests \u263E", () => {
+
+  const b = budo("../src/main.js");
+
+  it("should check document title", (done) => {
+    Nightmare().goto("http://127.0.0.1:9966")
+      .wait("#mount").evaluate(() => {
+        return document.title;
+      }).end().then((result) => {
+        expect(result).to.equal("BTT");
+        done();
+      });
+  });
+
+  it("should close test server", (done) => {
+    b.close();
+    done();
+  });
+});
