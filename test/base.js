6981be7f6c3833dac837f18e9c35308daacd38f8
Leonardo Silveira <sombriks@gmail.com>
2016-12-20 04:17:45 -0300

    .

--- test/base.js
+++ test/base.js
@@ -6,7 +6,7 @@ const budo = require("budo");
 
 describe("\u263C basic tests \u263E", () => {
 
-  const b = budo("../src/main.js");
+  const b = budo("./src/main.js");
 
   it("should check document title", (done) => {
     Nightmare().goto("http://127.0.0.1:9966")
